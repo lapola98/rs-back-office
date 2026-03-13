a3c70dccd46fbda0a0bcd4332975f645b157d835
Pola Blanco <48301113+lapola98@users.noreply.github.com>
2026-03-13 18:18:27 -0500

    supabase

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://doauzsmkoeyvllbmbdda.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_vMi1ZBvzXFMxrHvdlfmyBA_lhBCEAYn";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
